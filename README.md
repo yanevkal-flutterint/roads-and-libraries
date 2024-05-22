a87501ed0311b9334d0235c03931d6605e6d13c8
Kaloyan Yanev <kaloyan_yanev@flutterint.com>
2024-05-22 09:16:54 +0300

    Added skeleton for the solution

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # roads-and-libraries
+
+Link to original challenge:
+https://www.hackerrank.com/challenges/torque-and-development/problem?isFullScreen=true
---
+++ src/main/java/org/kaloyan/Main.java
@@ -0,0 +1,61 @@
+package org.kaloyan;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int c_lib = Integer.parseInt(firstMultipleInput[2]);
+
+                int c_road = Integer.parseInt(firstMultipleInput[3]);
+
+                List<List<Integer>> cities = new ArrayList<>();
+
+                IntStream.range(0, m).forEach(i -> {
+                    try {
+                        cities.add(
+                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                        .map(Integer::parseInt)
+                                        .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                long result = roadsAndLibraries(n, c_lib, c_road, cities);
+                System.out.println(result);
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+
+    // n stands for numberOfCities
+    public static long roadsAndLibraries(int n, int libraryCost, int roadCost, List<List<Integer>> potentialRoads) {
+
+        return 1L;
+    }
+}
